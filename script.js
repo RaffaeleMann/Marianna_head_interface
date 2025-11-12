5a27cdc091eee55f328e2d3d77236f22aa75f487
RaffaeleMann <44782174+RaffaeleMann@users.noreply.github.com>
2025-11-12 16:48:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ async function getTextResponse() {
     const data = await res.json();
 
     if (data.summary) {
-  output.innerHTML = `<img src="img/marianna_pxl.png" alt="Marianna" class="response-icon"> ${data.summary}`;
+      output.textContent = `🗣️ ${data.summary}`;
     } else if (data.transcription) {
       output.textContent = `Trascrizione: ${data.transcription}`;
     } else {
